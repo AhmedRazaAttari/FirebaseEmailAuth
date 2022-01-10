996ebb478f0cf9652bfd4260b90cd33a683d7e8f
Ahmed Raza <49317114+AhmedRazaAttari@users.noreply.github.com>
2022-01-10 11:53:59 -0800

    Delete server directory

--- server/route/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-var cors = require('cors');
-router.use(cors());
-
-router.use('/steps', require('./Steps'));
-
-module.exports = router;
